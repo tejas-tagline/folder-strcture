5093b88fee972872e7b379b4d8ec8546dc8df96e
tejas-tagline <tejas.tagline@gmail.com>
2021-11-30 18:53:17 +0530

    added comments

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,49 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { NodeModel } from 'src/app/models/node.model';
+import { Component, OnInit } from "@angular/core";
+import { NodeModel } from "src/app/models/node.model";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
   folderList: NodeModel[] = [];
   isAddRootFolder: boolean = false;
   isAddFolder: boolean = false;
   isAddNode: boolean = false;
-  selectedFileType!: 'folder' | 'file' | 'unset' | null;
+  selectedFileType!: "folder" | "file" | "unset" | null;
   imgsrc!: string;
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  // Manage show/hide Add to root folder input box
   showInput(): void {
     this.isAddRootFolder = !this.isAddRootFolder;
   }
 
+  // To add folder to the root
   addRootFolder(name: string): void {
-    if (name) this.folderList.push({ type: 'file', name: name });
+    if (name) this.folderList.push({ type: "file", name: name });
     this.isAddRootFolder = false;
   }
 
+  // Manage add/remove nodes
   manageNode(type: string): void {
     switch (type) {
-      case 'add':
+      case "add":
         this.isAddNode = true;
         break;
-      case 'remove':
+      case "remove":
         this.folderList = [];
         break;
-      case 'default':
+      case "default":
         break;
     }
   }
 
+  // To add node file/folder to the folder
   addNodeFileFolder(name: string): void {
     if (name) {
       let arr: NodeModel[] = [];
       arr.push({ name: name, type: this.selectedFileType });
+      // check if array does has childern array or not, if there is no childern array then assign array else push the value to the children array.
       if (!this.folderList[0].children) {
         this.folderList[0].children = arr;
       } else {
@@ -57,12 +62,13 @@ export class DashboardComponent implements OnInit {
     this.isAddNode = false;
   }
 
-  addFile(type: 'folder' | 'file' | 'unset' | null): void {
+  // show/hide Add file/folder to the Folder
+  addFile(type: "folder" | "file" | "unset" | null): void {
     this.isAddFolder = true;
     this.selectedFileType = type;
     this.imgsrc =
-      type === 'file'
-        ? 'assets/file-regular.svg'
-        : 'assets/folder-open-regular.svg';
+      type === "file"
+        ? "assets/file-regular.svg"
+        : "assets/folder-open-regular.svg";
   }
 }
